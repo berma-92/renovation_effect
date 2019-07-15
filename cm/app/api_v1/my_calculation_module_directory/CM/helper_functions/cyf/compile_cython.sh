4a17e128cd6614bb6501532a43ffcc39b8731625
fallahnejad <fallahnejad@eeg.tuwien.ac.at>
2019-07-15 14:55:22 +0200

    amend the path to the compile_cython_file.py

--- cm/app/api_v1/my_calculation_module_directory/CM/helper_functions/cyf/compile_cython.sh
+++ cm/app/api_v1/my_calculation_module_directory/CM/helper_functions/cyf/compile_cython.sh
@@ -1,3 +1,3 @@
-python -m cython -a create_density_map.pyx
+#python -m cython -a create_density_map.pyx
 
-#python compile_cython_files.py build_ext --inplace
+python ./app/api_v1/my_calculation_module_directory/CM/helper_functions/cyf/compile_cython_files.py build_ext --inplace
