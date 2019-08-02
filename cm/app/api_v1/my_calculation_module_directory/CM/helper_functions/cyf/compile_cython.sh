66b06adf3b20c4f898ca8440fcee370f2dc0034c
fallahnejad <fallahnejad@eeg.tuwien.ac.at>
2019-08-02 16:58:40 +0200

    use python3 instead of python for cython

--- cm/app/api_v1/my_calculation_module_directory/CM/helper_functions/cyf/compile_cython.sh
+++ cm/app/api_v1/my_calculation_module_directory/CM/helper_functions/cyf/compile_cython.sh
@@ -1,3 +1,3 @@
-python -m cython -a create_density_map.pyx
+python3 -m cython -a create_density_map.pyx
 
 #python ./app/api_v1/my_calculation_module_directory/CM/helper_functions/cyf/compile_cython_files.py build_ext --inplace
