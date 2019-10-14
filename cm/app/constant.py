81505f88407bc82da8cb8df72c7bdd22078fd9cf
Müller Andreas <andreasmueller284@gmail.com>
2019-10-14 06:16:41 +0200

    Category not all caps

--- cm/app/constant.py
+++ cm/app/constant.py
@@ -135,7 +135,7 @@ INPUTS_CALCULATION_MODULE = [
 
 SIGNATURE = {
 
-    "category": "DEMAND",
+    "category": "Demand",
     "authorized_scale":["NUTS 3", "NUTS 2", "NUTS 1","NUTS 0", "LAU 2", "Hectare"],
     "cm_name": CM_NAME,
     "description_link": "https://github.com/HotMaps/hotmaps_wiki/wiki/en-CM-Demand-projection",
