43cc4dff7f4682229227fb719be470b735d13d39
Müller Andreas <andreasmueller284@gmail.com>
2020-06-10 14:15:12 +0200

    Still testing to get pyx compile

--- cm/app/api_v1/my_calculation_module_directory/CM/N3scenario_to_raster.py
+++ cm/app/api_v1/my_calculation_module_directory/CM/N3scenario_to_raster.py
@@ -65,18 +65,20 @@ def CalcEffectsAtRasterLevel(NUTS_RESULTS_GFA_BASE
     
     try:
         import CM.helper_functions.cyf.create_density_map as CDM
+        asdf
     except:
         import pathlib
         p_ = pathlib.Path(__file__).parent.absolute()
-        ld = os.listdir("%s/helper_functions/cyf/" % p_)
+        d_ = "%s/helper_functions/cyf/" % p_
+        ld = os.listdir(d_)
         print(ld)
         
         python_interpret = sys.executable
         print(python_interpret)
         
-        os.system("%s %s/setup.py build_ext --inplace" %(python_interpret, p_))
+        os.system("%s %s/setup.py build_ext --inplace" %(python_interpret, d_))
         
-        ld = os.listdir("%s/helper_functions/cyf/" % p_)
+        ld = os.listdir(d_)
         print(ld)
         
         import CM.helper_functions.cyf.create_density_map as CDM
